a93d796b040551aa75111f1a132b848f8e7bd1a0
Peter Nguyen <ptn045@ucsd.edu>
2023-02-11 15:21:32 -0800

    make NutritionRow food submit function work with single general component

--- src/HabitsPanel/HabitsPanel.js
+++ src/HabitsPanel/HabitsPanel.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import NutritionRow from './NutritionRow';
+// import NutritionRow from './NutritionRow';
 import ExerciseRow from './ExerciseRow';
 import MobilizeRow from './MobilizeRow';
 import SleepRow from './SleepRow';
@@ -16,16 +16,20 @@ const HabitsPanel = (props) => {
 	const [hydrateScore, setHydrateScore] = useState(0);
 	const [reflectScore, setReflectScore] = useState(0);
 	const [reflectNotes, setReflectNotes] = useState('');
+	
+	// Specific states for Nutrition tracker
+	const [foodArray, setFoodArray] = useState([]);
+	const [nutritionPoints, setNutritionPoints] = useState(5);
 
 	const scoreUpdateHandler = (habit, points, data) => {
-		if (habit === 'SLEEP') {
+		if (habit === 'Sleep') {
 			setSleepScore(sleepScore + points);
 		} else if (habit === 'Hydrate') {
 			setHydrateScore(hydrateScore + points);
 		} else if (habit === 'Reflect') {
 			setReflectScore(reflectScore + points);
 			setReflectNotes(data);
-		} else if (habit === 'NUTRITION') {
+		} else if (habit === 'Nutrition') {
 			setNutritionScore(points);
 			setNutritionFoodArray(data);
 		}
@@ -47,16 +51,39 @@ const HabitsPanel = (props) => {
 		props.onSubmitDay(dayData); //pass day data to App.js
 	}
 
+	const nutritionAddHandler = (pointsSelected, enteredFood) => {
+		let foodEntry = {
+			points: pointsSelected,
+			food: enteredFood
+		};
+		let updatedArray = [foodEntry, ...foodArray];
+		let updatedPoints = nutritionPoints + +pointsSelected;
+
+		setFoodArray((prevFoodArray) => {
+			return [foodEntry, ...prevFoodArray];
+		});
+		setNutritionPoints(updatedPoints);
+		scoreUpdateHandler('Nutrition', updatedPoints, updatedArray);
+	};
+
 	return (
 		<form onSubmit={submitHandler}>
-			<NutritionRow onScoreUpdate={scoreUpdateHandler} />
+			{/* <NutritionRow onScoreUpdate={scoreUpdateHandler} /> */}
+			<TrackingRow 
+				name="Nutrition"
+				select={ {show: true, label: 'Pts', id: 'nutrition-select'} }
+				text={ {show: true, label: 'Food', id: 'nutrition-text'} }
+				button= { {show: true, label: 'Add'} }
+				buttonOnClick = {nutritionAddHandler}
+				onScoreUpdate={scoreUpdateHandler}
+				np={nutritionPoints}
+			/>
 			<ExerciseRow />
 			<MobilizeRow />
 			<SleepRow onScoreUpdate={scoreUpdateHandler} />
 			<TrackingRow
 				name="Hydrate"
 				checkbox={ {show: true, label: 'Accomplished', id: 'hydrate-check'} }
-				textarea={{show: false}}
 				onScoreUpdate={scoreUpdateHandler} />
 			<WellBeingRow />
 			<TrackingRow
--- src/HabitsPanel/TrackingRow.js
+++ src/HabitsPanel/TrackingRow.js
@@ -4,7 +4,9 @@ import Input from '../UI/Input';
 
 const TrackingRow = (props) => {
 	const [checkboxChecked, setCheckboxChecked] = useState(false);
-	// const [enteredText, setEnteredText] = useState('');
+	// const [enteredTextArea, setEnteredTextArea] = useState('');
+	const [enteredText, setEnteredText] = useState('');
+	const [selectSelected, setSelectSelected] = useState('-1');
 	
 	const checkboxHandler = () => {
 		if (!checkboxChecked) {
@@ -16,14 +18,31 @@ const TrackingRow = (props) => {
 		}
 	}
 
-	const textInputHandler = (event) => {
-		// setEnteredText(event.target.value);
+	const textAreaInputHandler = (event) => {
+		// setEnteredTextArea(event.target.value);
 		props.onScoreUpdate(props.name, 0, event.target.value);
 	}
-	
+
+	const textInputHandler = (event) => {
+		setEnteredText(event.target.value);
+		props.onScoreUpdate(props.name, 0, event.target.value);
+	}
+
+	const selectInputHandler = (event) => {
+		setSelectSelected(event.target.value);
+	}
+
+	const buttonClickHandler = (event) => {
+		event.preventDefault(); // prevent form submission
+		props.buttonOnClick(selectSelected, enteredText);
+	}
+
 	let checkboxInput = '';
 	let textareaInput = '';
 	let textInput = '';
+	let selectInput ='';
+	let buttonInput = '';
+	let npDisplay = '';
 	if (props.checkbox !== undefined && props.checkbox.show) {
 		checkboxInput =
 			<Input
@@ -38,11 +57,32 @@ const TrackingRow = (props) => {
 				type="textarea"
 				label={props.textarea.label}
 				id={props.textarea.id}
-				// value={enteredText}
-				onChange={textInputHandler} />;
+				// value={enteredTextArea}
+				onChange={textAreaInputHandler} />;
+	}
+	if (props.select !== undefined && props.select.show) {
+		selectInput =
+			<Input
+				type="select"
+				id={props.select.id}
+				label={props.select.label}
+				value={selectSelected}
+				onChange={selectInputHandler} />;
 	}
 	if (props.text !== undefined && props.text.show) {
-		textInput = <Input type="text" />;
+		textInput = 
+			<Input
+				type="text"
+				id={props.text.id}
+				label={props.text.label}
+				value={enteredText}
+				onChange={textInputHandler} />;
+	}
+	if (props.button !== undefined && props.button.show) {
+		buttonInput = <button onClick={buttonClickHandler}>{props.button.label}</button>;
+	}
+	if (props.np !== undefined) {
+		npDisplay = <p>{props.np}</p>;
 	}
 
 	return (
@@ -50,8 +90,11 @@ const TrackingRow = (props) => {
 			<h2>{props.name}</h2>
 			{checkboxInput}
 			{textareaInput}
+			{selectInput}
 			{textInput}
+			{buttonInput}
 			{checkboxChecked && <p>+5</p>}
+			{npDisplay}
 		</HabitRowWrapper>
 	);
 }
